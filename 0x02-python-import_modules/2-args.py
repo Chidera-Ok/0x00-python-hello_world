034ba3c3c10cbee812d71c30c31a96bc888c7739
Okeke Chidera <okekechidera98@gmail.com>
2022-12-14 11:41:43 +0000

    make corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
 
-    for i in range(1, count + 1):
-        print("{} : {}".format(i, sys.argv[i]))
+    for i in range(count):
+        print("{} : {}".format(i + 1, sys.argv[i + 1]))
